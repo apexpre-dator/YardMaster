5b6886ac01c4fb0c95aa57f052d4ea53c4735d31
Amartya Yadav <iamartyayadav@gmail.com>
2023-08-26 19:49:01 +0530

    fix

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:sidebarx/sidebarx.dart';
 import 'package:yms/methods/firestore_methods.dart';
